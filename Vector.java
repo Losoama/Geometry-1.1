a52b85abdb1def7d92eb482c83464c19cfc7463b
Losoama <igabaydulin@gmail.com>
2014-01-26 05:56:58 -0800

    Update Vector.java

--- Vector.java
+++ Vector.java
@@ -19,14 +19,35 @@ public class Vector {
         this.size = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
     }
 
+    public Vector(Point p0, Point p1) {
+        this.line = new Line(p0, p1);
+        this.point0 = p0;
+        this.point1 = p1;
+        this.x = this.point1.getX() - this.point0.getX();
+        this.y = this.point1.getY() - this.point0.getY();
+        this.size = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+
     public static Vector addVectors(Vector v1, Vector v2)
             throws IllegalArgumentException {
-        return new Vector(new Line());
+        if (!v1.point1.equals(v2)) {
+            throw new IllegalArgumentException("Конец первого вектора должен совпадать с началом второго вектора!");
+        }
+        return new Vector(new Line(v1.point0, v2.point1));
+    }
+
+    public static int mulVectors(Vector v1, Vector v2) {
+        return (v1.point1.getX() - v1.point0.getX()) * (v2.point1.getY() - v2.point0.getY()) -
+                (v1.point1.getY() - v1.point0.getY()) * (v2.point1.getX() - v2.point0.getX());
     }
 
     @Override
     public String toString() {
         return "Координата вектора: (" + this.x + ", " + this.y + ")\n" +
                 "Длина вектора: " + this.size + "\n";
+    }
+
+    public double getSize(){
+        return this.size;
     }
 }
